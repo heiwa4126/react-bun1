7d3f90d05e4d05583233df5472a48e2f0f1a1a94
heiwa4126 <heiwa4126@gmail.com>
2026-08-25 11:16:38 +0900

    add CDN

--- build.ts
+++ build.ts
@@ -1,14 +1,20 @@
+import { rm } from "node:fs/promises";
 import { basename } from "node:path";
 
 // same as
 // `bun build ./src/index.html --outdir ./dist --sourcemap linked --target browser --minify --define:process.env.NODE_ENV=production --env="BUN_PUBLIC_*"`
 // (but it's too long to type in the terminal, so we can use this script instead)
 
+const outdir = "./dist";
+
+await rm(outdir, { recursive: true, force: true });
+
 const startedAt = performance.now();
 const result = await Bun.build({
 	entrypoints: ["./src/index.html"],
-	outdir: "./dist",
-	sourcemap: "linked",
+	outdir,
+	// sourcemap: "linked",
+	sourcemap: "none",
 	target: "browser",
 	minify: true,
 	naming: {
@@ -18,7 +24,9 @@ const result = await Bun.build({
 		"process.env.NODE_ENV": JSON.stringify("production")
 	},
 	env: "BUN_PUBLIC_*",
-	metafile: true
+	metafile: true,
+	external: ["react", "react/jsx-runtime", "react-dom", "react-dom/client"],
+	drop: ["console", "debugger"]
 });
 
 if (!result.success) {
